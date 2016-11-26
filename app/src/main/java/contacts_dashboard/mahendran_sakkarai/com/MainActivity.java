073ac193192968aa68f22182a3caff05e05cb9e3
Mahendran Sakkarai <mahendranmails@gmail.com>
2016-11-27 00:56:30 +0530

    Initial Commit

---
+++ app/src/main/java/contacts_dashboard/mahendran_sakkarai/com/MainActivity.java
@@ -0,0 +1,13 @@
+package contacts_dashboard.mahendran_sakkarai.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
